a8dbfde89c13ca9c9f5b15326dd400010f7ede11
Victor Moura <viict@users.noreply.github.com>
2021-10-26 02:16:57 -0300

    no service to build

--- .circleci/process.sh
+++ .circleci/process.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 pwd
 SERVICES=`find ./ -name Makefile ! -path '*/node_modules/*'`
-JSON='{"stage": "pr", "builds":{}, "deploy": false}'
+
+STAGE="default"
+case $CIRCLE_BRANCH in
+    development)
+        STAGE=$CIRCLE_BRANCH
+    ;;
+    staging)
+        STAGE=$CIRCLE_BRANCH
+    ;;
+    master)
+        STAGE=$CIRCLE_BRANCH
+    ;;
+esac
+
+JSON='{"stage": "'$STAGE'", "builds":{}, "deploy": false}'
 
 for SERVICE in $SERVICES; do
     SERVICE_PATH=`echo $SERVICE | awk -F '/Makefile' '{ print $1 }' | awk -F '\./' '{ print $2 }'`
     echo "Service: $SERVICE_PATH on $SERVICE"
     FASTTRACK=`make --quiet -C $SERVICE_PATH fast_track`
 
-    JSON=`jq --arg service ${SERVICE_PATH} --arg value ${FASTTRACK} '. | .jobs[$service]=$value' <<< $JSON`
+    JSON=`jq --arg service ${SERVICE_PATH} --arg value ${FASTTRACK} '. | .builds[$service]=$value' <<< $JSON`
 done
 
-BUILDS=`echo $JSON | jq -r '.jobs|to_entries|map(select( (.value) != "FALSE" ))|map("      - build:
+BUILDS=`echo $JSON | jq -r '.builds|to_entries|map(select( (.value) != "FALSE" ))|map("      - build:
           name: \"Build \(.key)\"
           service_name: \(.key)
           requires:
             - checkout")|.[]'`
-TESTS_REQUIRED=`echo $JSON | jq -r '.jobs|to_entries|map(select( (.value) != "FALSE" ))|map("            - \"Build \(.key)\"")|.[]'`
+TESTS_REQUIRED=`echo $JSON | jq -r '.builds|to_entries|map(select( (.value) != "FALSE" ))|map("            - \"Build \(.key)\"")|.[]'`
 
 echo $JSON | jq
 
