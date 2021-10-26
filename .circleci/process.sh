d9ff0ea953287098d6b20793969bd38bb44d44d2
Victor Moura <viict@users.noreply.github.com>
2021-10-26 01:40:06 -0300

    apply fast_track

--- .circleci/process.sh
+++ .circleci/process.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+pwd
+SERVICES=`find ./ -name Makefile ! -path '*/node_modules/*'`
+JSON='{"stage": "pr", "builds":{}, "deploy": false}'
 
-env
+for SERVICE in $SERVICES; do
+    SERVICE_PATH=`echo $SERVICE | awk -F '/Makefile' '{ print $1 }' | awk -F '\./' '{ print $2 }'`
+    echo "Service: $SERVICE_PATH on $SERVICE"
+    FASTTRACK=`make --quiet -C $SERVICE_PATH fast_track`
 
-node --version
+    JSON=`jq --arg service ${SERVICE_PATH} --arg value ${FASTTRACK} '. | .jobs[$service]=$value' <<< $JSON`
+done
+
+echo $JSON
